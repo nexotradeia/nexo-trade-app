38f0569a64ff42a24a6ee285bbf21230f5a8863b
nexotradeia <mgtproia@gmail.com>
2026-06-01 17:02:23 -0400

    chore: deploy src/App.jsx 2026-06-01 17:02:21

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// NEXO TRADE — build: 2026-06-01 16:59:16
+// NEXO TRADE — build: 2026-06-01 17:02:21
 import { useState, useEffect, useRef, useContext, createContext, useCallback, useMemo } from 'react';
 import { createClient } from '@supabase/supabase-js';
 import * as THREE from 'three';
